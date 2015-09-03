07a4d2e843c41c3e2b5221f9a9e5c2775b664ffd
Roland Munsil <roland@munsil.com>
2015-09-03 15:24:53 -0500

    Renamed projects & namespaces

--- PixelWindowCSharpSDL/Class1.cs
+++ PixelWindowCSharpSDL/Class1.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using SDL2;
 
-namespace PixelWindowCSharpSDL
+namespace PixelWindowSDL
 {
     public struct Color
     {
--- PixelWindowCSharpSDLTester/Program.cs
+++ PixelWindowCSharpSDLTester/Program.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using PixelWindowCSharpSDL;
+using PixelWindowSDL;
 
-namespace PixelWindowCSharpSDLTester
+namespace PixelWindowTester
 {
     class Program
     {
