516a11d7fa34d29988818917cbfe81144dda8239
Roland Munsil <roland@munsil.com>
2015-09-07 19:05:19 -0500

    Yay lots of improvements!
    * Everything is now tested within an inch of it's life in
    PixelWindowTester
    * Added Color.EqualsIgnoreAlpha
    * Added PixelWindow.IsOpen
    * Renamed reverseYDirection to yAxisPointsUpwards
    * Window event handling/threading is now done properly
    * Width & height checks for PixelWindow constructor
    * Added PixelWindow.SaveClientToPNG()

--- PixelWindowSDL/PixelWindow.cs
+++ PixelWindowSDL/PixelWindow.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SDL2;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
 
 namespace PixelWindowSDL
 {
@@ -50,6 +53,13 @@ namespace PixelWindowSDL
             return (Color)obj == this;
         }
 
+        public bool EqualsIgnoreAlpha(Color color)
+        {
+            return this.red == color.red &&
+                   this.green == color.green &&
+                   this.blue == color.blue;
+        }
+
         public static bool operator ==(Color color1, Color color2)
         {
             return color1.red == color2.red &&
@@ -67,16 +77,44 @@ namespace PixelWindowSDL
     {
         public int ClientWidth { get; private set; }
         public int ClientHeight { get; private set; }
+        public bool IsOpen { get; private set; }
 
         private IntPtr pWindow;
         private IntPtr pRenderer;
-        private bool reverseYDirection;
-        public PixelWindow(int width, int height, bool reverseYDirection = false)
+        private bool yAxisPointsUpwards;
+
+        private Thread windowThread;
+        private bool windowDoneCreating;
+
+        //TODO: use the flags? http://wiki.libsdl.org/SDL_WindowFlags
+        public PixelWindow(int width, int height, bool yAxisPointsUpwards = false)
         {
+            if (width <= 0 || height <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Invalid width or height");
+            }
+
             ClientWidth = width;
             ClientHeight = height;
-            SDL.SDL_CreateWindowAndRenderer(width, height, 0, out pWindow, out pRenderer);
-            this.reverseYDirection = reverseYDirection;
+            this.yAxisPointsUpwards = yAxisPointsUpwards;
+            IsOpen = true;
+
+            windowDoneCreating = false;
+            windowThread = new Thread(delegate()
+            {
+                SDL.SDL_Init(SDL.SDL_INIT_EVERYTHING);
+                //TODO: why does ths work without calling SDL_Init?
+                if (-1 == SDL.SDL_CreateWindowAndRenderer(width, height, SDL.SDL_WindowFlags.SDL_WINDOW_ALLOW_HIGHDPI, out pWindow, out pRenderer))
+                {
+                    throw new Exception("Window and renderer creation failed.");
+                }
+                windowDoneCreating = true;
+                EventLoop();
+                //TODO: dispose stuff? Or wait for Dispose() call?
+            });
+            windowThread.Start();
+
+            while (!windowDoneCreating) ;
         }
 
         public void Dispose()
@@ -84,6 +122,7 @@ namespace PixelWindowSDL
             SDL.SDL_DestroyRenderer(pRenderer);
             SDL.SDL_DestroyWindow(pWindow);
             SDL.SDL_Quit();
+            this.IsOpen = false;
             GC.SuppressFinalize(this);
         }
 
@@ -110,7 +149,7 @@ namespace PixelWindowSDL
                 throw new ArgumentOutOfRangeException("Coordinate is not within the client dimensions");
             }
             SDL.SDL_SetRenderDrawColor(pRenderer, c.red, c.green, c.blue, c.alpha);
-            SDL.SDL_RenderDrawPoint(pRenderer, x, this.reverseYDirection ? (ClientHeight - 1) - y : y);
+            SDL.SDL_RenderDrawPoint(pRenderer, x, this.yAxisPointsUpwards ? (ClientHeight - 1) - y : y);
         }
 
         public bool IsWithinClient(int x, int y)
@@ -123,10 +162,32 @@ namespace PixelWindowSDL
             SDL.SDL_RenderPresent(pRenderer);
         }
 
-        //public void GetClientAsBitmap()
-        //{
-        //    SDL.SDL_RenderCopy(
-        //}
+        private void EventLoop()
+        {
+            while (true)
+            {
+                SDL.SDL_Event sdlEvent;
+                SDL.SDL_PollEvent(out sdlEvent);
+                if (sdlEvent.type == SDL.SDL_EventType.SDL_QUIT)
+                {
+                    IsOpen = false;
+                    return;
+                }
+            }
+        }
+
+        //http://stackoverflow.com/questions/22315980/sdl2-c-taking-a-screenshot
+        public void SaveClientToPNG(String filePath)
+        {
+            IntPtr pSurface = SDL.SDL_CreateRGBSurface(0, ClientWidth, ClientHeight, 32, 0x00ff0000, 0x0000ff00, 0x000000ff, 0xff000000);
+            SDL.SDL_Surface surface = (SDL.SDL_Surface)Marshal.PtrToStructure(pSurface, typeof(SDL.SDL_Surface));
+            SDL.SDL_Rect rect = new SDL.SDL_Rect { x = 0, y = 0, w = ClientWidth, h = ClientHeight };
+
+            SDL.SDL_RenderReadPixels(pRenderer, ref rect, SDL.SDL_PIXELFORMAT_ARGB8888, surface.pixels, surface.pitch);
+            SDL_image.IMG_SavePNG(pSurface, filePath);
+
+            SDL.SDL_FreeSurface(pSurface);
+        }
 
         //public void FillRect
     }
--- PixelWindowTester/Program.cs
+++ PixelWindowTester/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using PixelWindowSDL;
+using System.IO;
 
 namespace PixelWindowTester
 {
@@ -11,30 +12,99 @@ namespace PixelWindowTester
     {
         static void Main(string[] args)
         {
-            PixelWindow window = new PixelWindow(500, 500);
-            Random rand = new Random();
+            // ============ Test Color ============
+            Assert(new Color(0xFFEEDD) == new Color(0xFF, 0xEE, 0xDD));
 
-            for (int x = 0; x < 500; x++)
+            Color c = new Color(10, 20, 30, 40);
+            Assert(c.red == 10);
+            Assert(c.green == 20);
+            Assert(c.blue == 30);
+            Assert(c.alpha == 40);
+            Assert(c == new Color(10, 20, 30, 40));
+
+            Assert(new Color(0, 0, 0, 100).EqualsIgnoreAlpha(new Color(0, 0, 0, 200)));
+
+
+            //========= Test PixelWindow ==========
+            EnsureException<ArgumentOutOfRangeException>(() => new PixelWindow(-1, -1));
+            EnsureException<ArgumentOutOfRangeException>(() => new PixelWindow(0, 0));
+            EnsureException<ArgumentOutOfRangeException>(() => new PixelWindow(-1, 500));
+            EnsureException<ArgumentOutOfRangeException>(() => new PixelWindow(500, -1));
+            EnsureException<ArgumentOutOfRangeException>(() => new PixelWindow(0, 500));
+            EnsureException<ArgumentOutOfRangeException>(() => new PixelWindow(500, 0));
+
+            using (PixelWindow window = new PixelWindow(1000, 500))
             {
-                for (int y = 0; y < 500; y++)
+                Assert(window.ClientWidth == 1000);
+                Assert(window.ClientHeight == 500);
+                Assert(window.IsOpen);
+
+                EnsureException<ArgumentOutOfRangeException>(() => window.SetPixel(1001, 501, new Color()));
+                EnsureException<ArgumentOutOfRangeException>(() => window.SetPixel(1001, 250, new Color()));
+                EnsureException<ArgumentOutOfRangeException>(() => window.SetPixel(250, 501, new Color()));
+                EnsureException<ArgumentOutOfRangeException>(() => window.SetPixel(-1, 1, new Color()));
+                EnsureException<ArgumentOutOfRangeException>(() => window.SetPixel(-1, -1, new Color()));
+                EnsureException<ArgumentOutOfRangeException>(() => window.SetPixel(1, -1, new Color()));
+
+                for (int x = 0; x < window.ClientWidth; x++)
                 {
-                    window.SetPixel(x, y, new Color{
-                        red = 255, 
-                        green = 0, 
-                        blue = 0, 
-                        alpha = 0}
-                        );
+                    for (int y = 0; y < window.ClientHeight; y++)
+                    {
+                        window.SetPixel(x, y, Color.Red);
+                    }
                 }
+                window.UpdateClient();
+
+                Console.WriteLine("Client should be completely red.");
+                Console.ReadKey();
+
+                Console.WriteLine("Testing png saving...");
+                String filePath = "test image.png";
+                window.SaveClientToPNG(filePath);
+                Assert(File.Exists(filePath));
+                using (System.Drawing.Bitmap bm = new System.Drawing.Bitmap(filePath))
+                {
+                    Assert(bm.Width == window.ClientWidth);
+                    Assert(bm.Height == window.ClientHeight);
+                    for (int x = 0; x < window.ClientWidth; x++)
+                    {
+                        for (int y = 0; y < window.ClientHeight; y++)
+                        {
+                            var pix = bm.GetPixel(x, y);
+                            Assert(bm.GetPixel(x, y) == System.Drawing.Color.FromArgb(0xFF, 0xFF, 0, 0));
+                        }
+                    }
+                }
+                File.Delete(filePath);
+
+                for (int i = 0; i < 400; i++)
+                {
+                    window.SetPixel(i, i, Color.Green);
+                }
+                window.UpdateClient();
+
+                Console.WriteLine("Client should have a diagonal green line extending from the top left corner.");
+                Console.ReadKey();
+
+
+
+                Console.WriteLine("Waiting for window to close...");
+                while (window.IsOpen) ;
+                Console.WriteLine("Window closed");
             }
 
-            EnsureException<ArgumentOutOfRangeException>(() => window.SetPixel(501, 501, new Color()));
-            EnsureException<ArgumentOutOfRangeException>(() => window.SetPixel(501, 250, new Color()));
-            EnsureException<ArgumentOutOfRangeException>(() => window.SetPixel(250, 501, new Color()));
-            EnsureException<ArgumentOutOfRangeException>(() => window.SetPixel(-1, 1, new Color()));
-            EnsureException<ArgumentOutOfRangeException>(() => window.SetPixel(-1, -1, new Color()));
-            EnsureException<ArgumentOutOfRangeException>(() => window.SetPixel(1, -1, new Color()));
+            using (PixelWindow window = new PixelWindow(1000, 500, true))
+            {
+                for (int i = 0; i < 400; i++)
+                {
+                    window.SetPixel(i, i, Color.Green);
+                }
+                window.UpdateClient();
 
-            window.UpdateClient();
+                Console.WriteLine("Client should have a diagonal green line extending from the bottom left corner.");
+                Console.ReadKey();
+            }
+
             Console.ReadKey();
         }
 
@@ -53,6 +123,14 @@ namespace PixelWindowTester
             if (!exceptionThrown)
             {
                 throw new Exception("action did not throw exception");
+            }
+        }
+
+        static void Assert(bool expr)
+        {
+            if (!expr)
+            {
+                throw new Exception("assertion failed");
             }
         }
     }
